7b353b5b41d11e27c688e2a69a7747a1e485cc9e
kdekany <kristof.dekany@apex-it-services.eu>
2017-08-09 13:09:46 +0200

    + more basic stuff

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,14 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction()
     {
         // replace this example code with whatever you need
