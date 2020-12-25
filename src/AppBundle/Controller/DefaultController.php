412b1e33ce28d2e18b1069fd6fd6e9495c71c161
kdekany <kristof.dekany@apex-it-services.eu>
2020-12-25 11:23:40 +0100

    - update to symfony 5.2 - php min to 7.4

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,16 +4,17 @@ namespace AppBundle\Controller;
 
 use AppBundle\Model\Common\CharacterClass\Warrior;
 use AppBundle\Model\Common\Race\Elf;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Service\CharacterService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
-    public function indexAction()
+    public function indexAction(CharacterService $characterService)
     {
-        $player = $this->get('service.character')->generateCharacter(new Elf(), new Warrior());
+        $player = $characterService->generateCharacter(new Elf(), new Warrior());
 
         // replace this example code with whatever you need
-        return $this->render('AppBundle:Character:character.html.twig', [
+        return $this->render('Character/character.html.twig', [
             'char' => $player
         ]);
     }
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
@@ -22,9 +22,7 @@ require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
